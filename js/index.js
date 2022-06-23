a34c97e4775761feb035049fbf274886b9cf360d
BaraOT <arthurbaraski@gmail.com>
2022-06-23 21:59:50 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const myInfo = {
+  myName: "Baraladou Osuluku",
+  myHeight: "5.8 feet",
+  myCountry: "Nigeria",
+};
+
+
+alert ('My name is ' + myInfo.myName + '.' +' I am ' + myInfo.myHeight + ' tall and a citizen of ' + myInfo.myCountry);
